e601484f1dc20e207a8c3356c66e947e1d0de97a
Thomas Frey <joe@mitlinxlernen.ch>
2023-11-15 16:05:44 +0100

    Basic html, css Design implementiert

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-//import './style.css'
 import './scss/main.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app main .vite-content').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
