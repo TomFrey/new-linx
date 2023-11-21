9a74524814804afd038ba27887cbb31ae06b41c6
Thomas Frey <joe@mitlinxlernen.ch>
2023-11-21 12:11:41 +0100

    Animationen während dem Scrolling können konfiguriert werden. Nur einmal animieren oder mehrmals. Welcher Teil des Element muss im Vieport sein.

--- js/utils/viewportScrolling.js
+++ js/utils/viewportScrolling.js
@@ -1,31 +1,59 @@
-
-
+/**
+ * Jedes Element das die css Klasse 'animate-in-vieport' hat. Erhält, wenn das Element im Viewport ist die Klasse 'visible'.
+ * 
+ * Mögliche Konfigurationen:
+ * - Wenn zusätzlich die Klasse 'whole-element-in-viewport' am Element hängt, dann wird 'visible' erst gesetzt, wenn das 
+ *   ganze Element im viewport ist, sonst reicht die obere, linke Ecke.
+ * 
+ * - Wenn zusätzlich die Klasse 'animate-just-once' am Element hängt, dann wird 'visible' gesetzt, wenn das Element
+ *   im Viewport ist, die Klasse wird aber nie mehr entfernt. Somit wird das Element nur einmal animiert.
+ */
 function toggleClassWhenElementInViewport() {
     let elements = document.querySelectorAll('.animate-in-viewport');
 
     //Die obere, linke Ecke ist im Viewport, nicht das ganze Element
-    function isElementInViewport(element) {
+    function isUpperLeftCornerInViewport(element) {
         let rect = element.getBoundingClientRect();
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
             rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
             rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-            // für das ganze Element innerhalb des Viewports
-            // https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
-            // rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            // rect.right <= (window.innerWidth || document.documentElement.clientWidth);
         );
     }
-      
+
+    // Das ganze Element innerhalb des Viewports
+    // https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+    function isWholeElementInViewport(element) {
+        let rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function viewportCondition (element){
+        if(element.classList.contains('whole-element-in-viewport')){
+            return isWholeElementInViewport(element);
+        } else {
+            return isUpperLeftCornerInViewport(element);
+        }
+    }
+
     function toggleClasses() {
         elements.forEach((element) => {
-            if (isElementInViewport(element)) {
+            //Wenn Element im Viewport ist, 'visible' hinzufügen.
+            if (viewportCondition(element)) {
                 element.classList.add('visible');
             }
-            /* Else-Bedinung entfernen, um .visible nicht wieder zu löschen, wenn das Element den Viewport verlässt. */
-            else { 
-                element.classList.remove('visible');
+
+            //Wenn das Element mehrmals animiert werden soll, 'visible' wieder entfernen.
+            if (!element.classList.contains('animate-just-once')) {
+                if (!viewportCondition(element)) {
+                    element.classList.remove('visible');
+                }
             }
         })
     }
