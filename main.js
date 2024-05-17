3379a30013bb9bb207eb8293e63478eaa9c9a164
Thomas Frey <joe@mitlinxlernen.ch>
2024-05-17 09:43:32 +0200

    Unnötiges gelöscht, clean up

--- main.js
+++ main.js
@@ -3,18 +3,12 @@ import { toggleClassWhenElementInViewport, toggleClassWhenYPosReached } from './
 import { initNavigation } from './js/navigation.js'
 import { initContact } from './js/modules/handleContact.js'
 import { implementGoogleMaps } from './js/modules/googleMaps.js'
-//import { getEnviromentVariable } from './js/services/getEnvVariable.js'
 import '@material/web/checkbox/checkbox.js';
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/button/outlined-button.js';
 
-
-
-//getEnviromentVariable('GOOGLE_MAPS_API_KEY');
 initNavigation();
 toggleClassWhenYPosReached(40);
 toggleClassWhenElementInViewport();
 initContact();
 implementGoogleMaps();
-  
-
